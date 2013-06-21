0ccb1e732bfb90aafa3f429dfe87dc19986165ac
sabof <esabof@gmail.com>
2013-06-22 00:13:09 +0100

    basics

--- pleiades.js
+++ pleiades.js
@@ -1,7 +1,31 @@
-var Brush = function() { };
+var pl = {};
 
-Brush.prototype = {
-  constructor: Brush,
+// -----------------------------------------------------------------------------
+
+pl.Utils = {
+  random: function(min, max) {
+    return (
+      Math.floor(
+        Math.random() *
+          (max - min)) +
+        min);
+  }
+};
+
+// -----------------------------------------------------------------------------
+
+pl.Pleiades = function() { };
+
+pl.Pleiades.prototype = {
+
+};
+
+// -----------------------------------------------------------------------------
+
+pl.Brush = function() { };
+
+pl.Brush.prototype = {
+  constructor: pl.Brush,
   position: null,
   rectangle: function(width, height) { },
   line: function(length, direction) { }
@@ -9,10 +33,24 @@ Brush.prototype = {
 
 // -----------------------------------------------------------------------------
 
-var Generator = function(brush) {
+pl.generatorParts = {};
+
+pl.generatorParts.makeMove1 = function(/* optional */ vMin, vMax) {
+  vMin = vMin || 5;
+  vMax = vMax || 10;
+};
+
+// -----------------------------------------------------------------------------
+
+pl.Generator = function(brush) {
   this.brush = brush;
 };
 
-Generator.prototype = {
-  constructor: Generator
+pl.Generator.prototype = {
+  constructor: pl.Generator,
+  makeSequence: function(numberOfMoves) {
+
+  },
+  makeMove: function() { throw new Error('Not implemented'); }
+
 };
