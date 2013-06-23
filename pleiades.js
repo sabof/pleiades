7c81f60cfef00bcd6da8d1918c04aca930f19238
sabof <esabof@gmail.com>
2013-06-23 20:37:48 +0100

    don't redraw when window is hidden

--- pleiades.js
+++ pleiades.js
@@ -427,6 +427,40 @@ window.requestAnimFrame = (function(){
     };
 })();
 
+(function() {
+  var hidden = "hidden";
+
+  function onchange (evt) {
+    var v = 'visible', h = 'hidden',
+        evtMap = {
+          focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h
+        };
+
+    evt = evt || window.event;
+    if (evt.type in evtMap)
+      document.body.className = evtMap[evt.type];
+    else
+      document.body.className = this[hidden] ? "hidden" : "visible";
+  }
+
+  // Standards:
+  if (hidden in document)
+    document.addEventListener("visibilitychange", onchange);
+  else if ((hidden = "mozHidden") in document)
+    document.addEventListener("mozvisibilitychange", onchange);
+  else if ((hidden = "webkitHidden") in document)
+    document.addEventListener("webkitvisibilitychange", onchange);
+  else if ((hidden = "msHidden") in document)
+    document.addEventListener("msvisibilitychange", onchange);
+  // IE 9 and lower:
+  else if ('onfocusin' in document)
+    document.onfocusin = document.onfocusout = onchange;
+  // All others:
+  else
+    window.onpageshow = window.onpagehide = window.onfocus = window.onblur = onchange;
+
+})();
+
 // -----------------------------------------------------------------------------
 
 var generator = new pl.Generator(),
@@ -451,9 +485,11 @@ function zoom() {
 }
 
 function refresh() {
-  sequences = generator.make();
-  zoomLevel = 0;
-  brush.drawSequence(sequences);
+  if (document.body.className !== 'hidden') {
+    sequences = generator.make();
+    zoomLevel = 0;
+    brush.drawSequence(sequences);
+  }
   setTimeout(refresh, 2000);
 }
 
