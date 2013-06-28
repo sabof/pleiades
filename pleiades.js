a80b10eecd64288f0a7ca1e16ce9e88948615865
sabof <esabof@gmail.com>
2013-06-28 18:17:59 +0100

    3.2

--- pleiades.js
+++ pleiades.js
@@ -47,12 +47,21 @@ var pl = {debug: false};
         min);
   };
 
-  var rotateArray = function(array, reverse) {
+  var rotateArray = function(array, ammount) {
+    if (ammount === undefined) {
+      ammount = 1;
+    }
     array = array.slice(0);
-    if (reverse) {
-      array.push(array.shift());
+    if (ammount < 0) {
+      while (ammount !== 0) {
+        array.push(array.shift());
+        ammount++;
+      }
     } else {
-      array.unshift(array.pop());
+      while (ammount !== 0) {
+        array.unshift(array.pop());
+        ammount--;
+      }
     }
     return array;
   };
@@ -246,7 +255,7 @@ var pl = {debug: false};
     },
 
     rotate: function(reverse) {
-      this.directions = rotateArray(this.directions, reverse);
+      this.directions = rotateArray(this.directions, reverse ? -1 : undefined);
     },
 
     reflect: function(across) {
@@ -371,7 +380,6 @@ var pl = {debug: false};
           this.mask[3] ])
           .attr({'stroke-width': 4, 'stroke': 'blue'});
       }
-
       this._walker(composition);
       if (pl.debug) {
         this._drawBoundingBox();
@@ -589,8 +597,7 @@ var pl = {debug: false};
         var newPoint = this.point;
 
         var adjPoint = this.translatePoint(this.point);
-        if (rectanglesOverlap(this.mask, pointsToRect(oldPoint, newPoint)))
-        {
+        if (rectanglesOverlap(this.mask, pointsToRect(oldPoint, newPoint))) {
           this.paper.rect.apply(
             this.paper,
             pointsToRect(adjOldPoint, adjPoint)
@@ -628,6 +635,15 @@ var pl = {debug: false};
   // -----------------------------------------------------------------------------
 
   pl.stampFactory = {
+    reset: function() {
+      this.recipes.largeCircle.iterator =
+        makeLooper(rotateArray(['.', 'none', '--', 'none'], random(4)));
+      this.makeMake();
+    },
+
+    init: function() {
+      this.reset();
+    },
 
     makeMake: function() {
       var self = this,
@@ -647,7 +663,7 @@ var pl = {debug: false};
       this.make = function(option) {
         var object = option ? this.recipes[option] :
             wheel[random(wheelLength)];
-        var result = object.func.call(this);
+        var result = object.func();
         return result;
       };
     },
@@ -707,25 +723,23 @@ var pl = {debug: false};
       largeCircle: {
         probability: 2,
         maxLength: 1,
-        func: (function() {
-          var iterator = makeLooper(['.', 'none', '--', 'none']);
-          return function() {
-            var dasharray = iterator();
-            var circle = [
-              'circle',
-              random(10, 300),
-              { 'stroke-width': (dasharray === 'none') ? 1 : 2,
-                'stroke-dasharray' : dasharray,
-                'stroke': 'white'
-                // 'stroke-opacity': 1
-                // 'fill-opacity': random(),
-                // 'fill': random('color')
-              } ];
-            circle.dontMeasure = true;
-            return circle; };
-
-        }())
-
+        iterator: makeLooper(rotateArray(['.', 'none', '--', 'none'],
+                                         random(4))),
+        func: function() {
+          var dasharray = this.iterator();
+          var circle = [
+            'circle',
+            random(10, 300),
+            { 'stroke-width': (dasharray === 'none') ? 1 : 2,
+              'stroke-dasharray' : dasharray,
+              'stroke': 'white'
+              // 'stroke-opacity': 1
+              // 'fill-opacity': random(),
+              // 'fill': random('color')
+            } ];
+          circle.dontMeasure = true;
+          return circle;
+        }
       },
 
       smallCircle: {
@@ -893,7 +907,7 @@ var pl = {debug: false};
         } else {
           pl.stampFactory.recipes.largeCircle.probability = 0;
         }
-        pl.stampFactory.makeMake();
+        pl.stampFactory.reset();
 
         for (var j = 0, jL = this.sequencesLength; j < jL; j++) {
           currentSequence.push(pl.stampFactory.make());
